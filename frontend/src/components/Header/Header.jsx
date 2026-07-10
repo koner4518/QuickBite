bf4ad57345fa25d01b588271bb6b8e83c458e8f0
Supriyo Koner <supriyokoner2005@gmail.com>
2026-07-11 03:22:44 +0530

    updated header image

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import './Header.css'
+import {assets} from '../../assets/assets.js'
 
 export default function Header(){
     return (
-        <div className='header'>
+        <div className='header' >
             <div className="header-contents">
                 <h2>Order your favorite food here</h2>
                 <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
